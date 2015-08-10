8a37619e43576c0c2ac3b4e5cb3c5fce5e52cf09
mr-flannery <dennis_hempfing@gmx.de>
2015-08-10 14:36:15 +0200

    Add form to create new page within the application.

--- update_cycle/urls.py
+++ update_cycle/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^detail/(?P<page_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^new/$', views.new, name='new'),
+    url(r'^create/$', views.create, name='create'),
 ]
--- update_cycle/views.py
+++ update_cycle/views.py
@@ -14,3 +14,15 @@ def index(request):
 def detail(request, page_id):
 	page = get_object_or_404(Page, pk=page_id)
 	return HttpResponse("This page has the ID %s." % page_id)
+
+def new(request):
+	return render(request, 'update_cycle/new.html')
+
+def create(request):
+	if request.method == 'POST':
+		p = Page(name=request.POST['name'], url=request.POST['url'], comment=request.POST['comment'], 
+			contact_person=request.POST['contact_person'], next_update_at=request.POST['next_update_at'])
+		p.save()
+		return index(request)
+	else:
+		return new(request)
